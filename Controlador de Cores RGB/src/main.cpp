2e0036dbdbed5ee5d9204155311a9fe596218b71
Matheus Renó Torres <Matheusrenor@gmail.com>
2024-06-08 11:37:54 -0300

    Init ADC and PWM

---
+++ Controlador de Cores RGB/src/main.cpp
@@ -0,0 +1,89 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/delay.h>
+
+#define pwmOut (1 << PD6)
+#define RedPin (1 << PD4)
+#define GreenPin (1 << PD5)
+#define BluePin (1 << PD7)
+
+unsigned int adc_result0;
+unsigned int adc_result1;
+unsigned int adc_result2;
+
+
+float tensao;
+
+void ADC_init(void) {
+  // Configurando Vref para VCC = 5V
+  ADMUX = (1 << REFS0);
+  /*
+    ADC ativado e preescaler de 128
+    16MHz / 128 = 125kHz
+    ADEN = ADC Enable, ativa o ADC
+    ADPSx = ADC Prescaler Select Bits
+    1 1 1 = clock / 128
+  */
+  ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+}
+
+int ADC_read(uint8_t ch) {
+  char i;
+  int ADC_temp = 0;  // ADC temporário, para manipular leitura
+  int ADC_read = 0;  // ADC_read
+  ch &= 0x07;        // 0b00000111
+  // Zerar os 3 primeiros bits e manter o resto
+  ADMUX = (ADMUX & 0xF8) | ch;  // 0b11111000
+  // ADSC (ADC Start Conversion)
+  ADCSRA |= (1 << ADSC);  // Faça uma conversão
+  // ADIF (ADC Interrupt Flag) é setada quando o ADC pede interrupção
+  // e resetada quando o vetor de interrupção
+  // é tratado.
+  while (!(ADCSRA & (1 << ADIF)))
+    ;                      // Aguarde a conversão do sinal
+  for (i = 0; i < 8; i++)  // Fazendo a conversão 8 vezes para maior precisão
+  {
+    ADCSRA |= (1 << ADSC);  // Faça uma conversão
+    while (!(ADCSRA & (1 << ADIF)))
+      ;                       // Aguarde a conversão do sinal
+    ADC_temp = ADCL;          // lê o registro ADCL
+    ADC_temp += (ADCH << 8);  // lê o registro ADCH
+    ADC_read += ADC_temp;     // Acumula o resultado (8 amostras) para média
+  }
+  ADC_read = ADC_read >> 3;  // média das 8 amostras ( >> 3 é o mesmo que /8)
+  return ADC_read;
+}
+
+void setRGB(float r, float g, float b) {
+  OCR0A = r / 4;  // Red
+  OCR0B = g / 4;  // Green
+  OCR0C = b / 4;  // Blue
+}
+
+int main() {
+
+  ADC_init();  // Inicializa ADC     
+
+  DDRD |= pwmOut;
+  PORTD &= ~pwmOut;
+
+  DDRD |= RedPin | GreenPin | BluePin;  // Configurando os pinos do LED RGB como saída
+
+  TCCR0A |= (1 << WGM01) | (1 << WGM00) | (1 << COM0A1);
+  TCCR0B |= (1 << CS00);
+  OCR0A = 0;
+
+  while (1) {
+    adc_result0 = ADC_read(ADC2D);  // lê o valor do ADC0 = PC0
+    adc_result1 = ADC_read(ADC3D);  // lê o valor do ADC1 = PC1
+    adc_result2 = ADC_read(ADC4D);  // lê o valor do ADC2 = PC2
+    _delay_ms(50);                  // Tempo para troca de canal
+
+    float r = adc_result0 / 4;  // Conversão do valor do ADC para um valor entre 0 e 255
+    float g = adc_result1 / 4;  // Conversão do valor do ADC para um valor entre 0 e 255
+    float b = adc_result2 / 4;  // Conversão do valor do ADC para um valor entre 0 e 255
+
+    setRGB(r, g, b);  // Configura a cor do LED RGB
+  }
+  return 0;
+}
